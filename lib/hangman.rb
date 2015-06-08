a2665a64b7dd078fcb1a81ec3b0b62d25d1a6c59
Steven Havko <stevenhavko@gmail.com>
2015-06-08 16:34:21 -0400

    working game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 def pick_random_line
   chosen_line = nil
   File.foreach("5desk.txt").each_with_index do |line, number|
@@ -14,15 +15,25 @@ win = false
 display = Array.new((word.length - 1), '_')
 incorrect = []
 puts word
-
-
-
+puts "would you like to load a game"
+answer = gets.chomp.downcase
+if answer == 'yes'
+   content = File.open('games/saved.yaml', 'r') { |file| file.read }
+  YAML.load(content) # returns a Hangman object
+elsif answer == 'no'
+  
   while (guesses != 0 && win == false)
   puts display
   puts "Wrong guesses #{incorrect}"
   puts "Enter your letter"
   guess = gets.chomp
-  if word_array.none? { |w| w == guess} # add unfound letter to misses array, reduce turns
+  if guess == 'save'
+     Dir.mkdir('games') unless Dir.exist? 'games'
+  filename = 'games/saved.yaml'
+  File.open(filename, 'w') do |file|
+    file.puts YAML.dump(self)
+  end
+  elsif word_array.none? { |w| w == guess} # add unfound letter to misses array, reduce turns
       incorrect << guess
       guesses -= 1
       puts "You have #{guesses} left"
@@ -30,13 +41,18 @@ puts word
     word_array.each_with_index do |letter, i| # add letter to display array
       display[i] = letter if letter == guess
     end
+     if display.none? { |i| i == '_' }
+        result = "#{display.join('  ')}\nCongrats, you win!"
+        win = true
+    end
+    puts result
 end
 
 if guesses == 0
 	puts "You Lose, the word was #{word}"
 end
 end
-
+end
 
 
  
